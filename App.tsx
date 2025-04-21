e38e8fed3e7396dd89d67d8e130688c2d8089b92
Iresh <iresh@blackswan-technologies.com>
2025-04-21 14:44:20 +0530

    remove unwanted

--- App.tsx
+++ App.tsx
@@ -11,7 +11,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { LoginScreen } from './src/screens/LoginScreen';
 import { HomeScreen } from './src/screens/HomeScreen';
-import { initializeFirebase } from './src/services/firebase';
 
 const Stack = createNativeStackNavigator();
 
